bc90735777fe13df0bb764ff5877f490b8818e57
Xeonanda <varenxeon789@gmail.com>
2025-05-16 14:29:22 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/message', function () {
+    return response()->json(['message' => 'This is your message']);
+});
