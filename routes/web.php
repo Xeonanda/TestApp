56e8420b76f2849057874a6d7f8b98091e3d33f4
Xeonanda <varenxeon789@gmail.com>
2025-05-17 12:56:15 +0700

    api rework

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/message', function () {
-    return response()->json(['message' => 'This is your message']);
-});
